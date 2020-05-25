75475d2d78cd3d50d89b696f66b2e85afdacd0f5
Hassan Abdin <habdin@gmail.com>
2020-05-25 14:50:14 +0200

    Blogger Templates implemented

--- blog/routes.py
+++ blog/routes.py
@@ -3,6 +3,7 @@
 
 """Module for defining various view functions for the Blogger Flask App."""
 
+from flask import render_template
 from blog import app
 
 
@@ -10,4 +11,17 @@ from blog import app
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     """Home page view function"""
-    return '<h1>Hello, World!</h1>'
+    # Fake user and posts to be removed when true elements of their kinds
+    # in the app are created.
+    user = {'username': 'habdin', 'first_name': 'Hassan'}
+    posts = [
+        {
+            'author': {'username': 'habdin', 'first_name': "Hassan"},
+            'body': 'I hope you like your visit to Egypt.'
+        },
+        {
+            'author': {'username': 'jdoe', 'first_name': 'John'},
+            'body': 'The weather in Cairo is nice today.'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
